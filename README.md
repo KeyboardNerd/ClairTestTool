0d60225251b63e697ef492339ff306897884254f
Sida Chen <sida.chen@coreos.com>
2017-08-15 13:32:19 -0400

    updated readme, fixed getImage bug

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PyClairAnalyzer
 python Clair client to post/get images and interact with the notifications as webhook.
+
+This works with Clair's V2 API.
--- getImages.py
+++ getImages.py
@@ -11,12 +11,13 @@ if __name__ == "__main__":
     clairVersion = int(sys.argv[1])
     images = sys.argv[2:]
     util.pullImages(images)
+    # concurrently gets image from Clair.
     p = Pool(len(images))
     if clairVersion == 1:
-        map(api.mapGetImageV1, images)
+        p.map(api.mapGetImageV1, images)
     elif clairVersion == 2:
-        map(api.mapGetImageV2, images)
+        p.map(api.mapGetImageV2, images)
     elif clairVersion == 3:
-        map(api.mapGetImageV3, images)
+        p.map(api.mapGetImageV3, images)
     else:
         exit(-1)
